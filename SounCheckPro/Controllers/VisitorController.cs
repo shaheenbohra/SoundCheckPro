20243e135f48139b6753d81443718dbb160dabc0
shaheenbohra <43819905+shaheenbohra@users.noreply.github.com>
2018-11-22 17:52:58 +0530

    --commit

---
+++ SounCheckPro/Controllers/VisitorController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SounCheckPro.Controllers
+{
+    public class VisitorController : Controller
+    {
+        // GET: Visitor
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+        public ActionResult SearchTalent()
+        {
+            return View();
+        }
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        public ActionResult Signin()
+        {
+            return View();
+        }
+    }
+}
