2efa193092e5fe392aebaec5d0226dcb071bf7a4
shaheenbohra <43819905+shaheenbohra@users.noreply.github.com>
2018-11-23 14:51:56 +0530

    --admin

---
+++ SounCheckPro/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SounCheckPro.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AdminDashboard()
+        {
+            return View();
+        }
+        public ActionResult CheckTalent()
+        {
+            return View();
+        }
+    }
+}
